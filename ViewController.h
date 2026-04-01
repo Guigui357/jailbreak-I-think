019ec4f8d8918b687905e389dfbe76824efcc4c6
Guigui357 <zebuchinho77@gmail.com>
2026-04-01 12:57:58 -0300

    Add WKWebView property to ViewController
    Added WKWebView property to ViewController interface

--- ViewController.h
+++ ViewController.h
@@ -1,1 +1,9 @@
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
+@interface ViewController : UIViewController
+
+// Definimos a webView aqui para que possamos acessá-la no .m
+@property (nonatomic, strong) WKWebView *webView;
+
+@end
