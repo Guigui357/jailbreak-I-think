804f20a246c26662715d798ee44d770fdfaea3fa
Guigui357 <zebuchinho77@gmail.com>
2026-04-01 13:32:14 -0300

    Refactor viewDidLoad for better clarity and structure

--- ViewController.m
+++ ViewController.m
@@ -6,22 +6,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    // 1. Forçar o carregamento da Bridge (Dylib)
-    // No A13, o Feather pode colocar a dylib em caminhos diferentes
+    // 1. Forçar carregamento da dylib (GitHub/Feather)
     void *handle = dlopen("@executable_path/KernelBridge.dylib", RTLD_NOW);
-    if (!handle) {
-        handle = dlopen("KernelBridge.dylib", RTLD_NOW);
+    if (!handle) handle = dlopen("KernelBridge.dylib", RTLD_NOW);
+
+    // 2. CONFIGURAÇÃO CRÍTICA: Registrar o handler ANTES da WebView
+    WKUserContentController *userContent = [[WKUserContentController alloc] init];
+    
+    // Tenta achar a classe KernelBridge que compilamos no GitHub
+    Class bridgeClass = NSClassFromString(@"KernelBridge");
+    if (bridgeClass) {
+        id bridgeInstance = [[bridgeClass alloc] init];
+        
+        // IMPORTANTE: O nome aqui DEVE ser "kexec" para bater com o seu JS
+        [userContent addScriptMessageHandler:bridgeInstance name:@"kexec"];
+        
+        // Conecta a webview à bridge para o C poder mandar logs de volta
+        // Usamos delay porque a webview ainda não foi criada
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [bridgeInstance setValue:self.webView forKey:@"webView"];
+        });
     }
 
-    // 2. Configurar o WKWebView e o Handler "kexec"
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-    
-    // IMPORTANTE: Criamos a instância da classe que está no KernelDriver.m
-    id bridge = [[NSClassFromString(@"KernelBridge") alloc] init];
-    if (bridge) {
-        [bridge setValue:self.webView forKey:@"webView"];
-        [config.userContentController addScriptMessageHandler:bridge name:@"kexec"];
-    }
+    config.userContentController = userContent;
 
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
     [self.view addSubview:self.webView];
@@ -29,7 +37,8 @@
     // 3. Carregar o HTML
     NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
     if (path) {
-        [self.webView loadFileURL:[NSURL fileURLWithPath:path] allowingReadAccessToURL:[NSURL fileURLWithPath:[path stringByDeletingLastPathComponent]]];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        [self.webView loadFileURL:url allowingReadAccessToURL:[url URLByDeletingLastPathComponent]];
     }
 }
 @end
