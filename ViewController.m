1b4d5807f8c82d17ef280301f97bb75046ac27eb
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 19:40:29 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -11,22 +11,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // Certifique-se que o driver é uma PROPRIEDADE (strong) da classe
     self.driver = [[KernelDriver alloc] init];
     
-    WKUserContentController *userCC = [[WKUserContentController alloc] init];
-    
-    // USAR EXATAMENTE ESTE MÉTODO (iOS 14+)
-    [userCC addScriptMessageHandlerWithReply:self.driver 
-                                contentWorld:[WKContentWorld pageWorld] 
-                                        name:@"kernel"];
-    
+    // 1. Criar a configuração PRIMEIRO
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-    config.userContentController = userCC;
     
+    // 2. Injetar o Handler DIRETAMENTE no controller da config
+    [config.userContentController addScriptMessageHandlerWithReply:self.driver 
+                                                      contentWorld:[WKContentWorld pageWorld] 
+                                                              name:@"kernel"];
+    
+    // 3. Habilitar privilégios de arquivo (necessário para o exploit ler o bundle)
+    [config setValue:@YES forKey:@"allowUniversalAccessFromFileURLs"];
+
+    // 4. Instanciar a WebView com a config JÁ POPULADA
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
     [self.view addSubview:self.webView];
     
     NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
     [self.webView loadFileURL:url allowingReadAccessToURL:url.URLByDeletingLastPathComponent];
 }
-@end
