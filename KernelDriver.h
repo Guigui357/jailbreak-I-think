3971d0bc86a54a1cb4c134103b59dc82fc881e22
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 18:12:55 -0300

    Update KernelDriver.h

--- KernelDriver.h
+++ KernelDriver.h
@@ -1,14 +1,12 @@
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
 
-#define ARM_TTE_PTE_MASK            0x0000FFFFFFFFF000ULL
-#define ARM_PTE_AP_RO               (1ULL << 6)   // Bit de Read-Only
-#define ARM_PTE_NX                  (1ULL << 54)  // Execute Never
-#define ARM_PTE_PNX                 (1ULL << 53)  // Privileged Execute Never
-
+// CERTIFIQUE-SE DE QUE O NOME DO PROTOCOLO ESTÁ EXATAMENTE ASSIM:
 @interface KernelBridge : NSObject <WKScriptMessageHandlerWithReply>
-- (uint64_t)kread64:(uint64_t)addr;
-- (void)kwrite64:(uint64_t)addr value:(uint64_t)val;
-- (uint64_t)get_pte_for_address:(uint64_t)vaddr;
-- (void)patch_pte_make_writable:(uint64_t)vaddr;
+
+// Assinatura do método que o WebKit procura para habilitar a função no JS
+- (void)userContentController:(WKUserContentController *)userContentController 
+      didReceiveScriptMessage:(WKScriptMessage *)message 
+                 replyHandler:(void (^)(id _Nullable reply, NSString * _Nullable errorMessage))replyHandler;
+
 @end
