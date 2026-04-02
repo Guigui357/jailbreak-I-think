5590eedab64fc17fbb3ba264ee938af31e6259bd
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 13:43:58 -0300

    Update ViewController.h

--- ViewController.h
+++ ViewController.h
@@ -1,9 +1,13 @@
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
+#import "KernelDriver.h" // Importante para o ViewController conhecer o Driver
 
 @interface ViewController : UIViewController
 
-// Definimos a webView aqui para que possamos acessá-la no .m
+// A WKWebView que exibirá o console hacker (index.html)
 @property (nonatomic, strong) WKWebView *webView;
+
+// A instância do motor que processará o kread64 e o SSHD
+@property (nonatomic, strong) KernelBridge *kernelBridge;
 
 @end
