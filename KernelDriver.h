257f50bb37d5b17020300f79b54625ba065bca18
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 19:07:58 -0300

    Update KernelDriver.h

--- KernelDriver.h
+++ KernelDriver.h
@@ -5,14 +5,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface KernelDriver : NSObject <WKScriptMessageHandlerWithReply>
 
-// Declaração dos métodos de Exploit
+// Primitivas de Exploração
 - (uint64_t)kread64:(uint64_t)addr;
+- (uint64_t)getKernelSlide;
 - (uint64_t)get_pte_for_address:(uint64_t)vaddr;
 - (uint64_t)get_my_ucred_ptr;
 - (void)ppl_write_race:(uint64_t)vaddr value:(uint64_t)val;
-- (uint64_t)getKernelSlide;
 
-// Assinatura correta do protocolo WKWebView
+// Handler da Ponte (Habilita postMessageWithReply no JS)
 - (void)userContentController:(WKUserContentController *)userContentController 
       didReceiveScriptMessage:(WKScriptMessage *)message 
                  replyHandler:(void (^)(id _Nullable reply, NSString * _Nullable errorMessage))replyHandler;
