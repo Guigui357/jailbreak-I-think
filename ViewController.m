fa53fea2dde23eaf64c888fbaae334e02821ed65
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 13:43:24 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -1,44 +1,42 @@
 #import "ViewController.h"
-#import <dlfcn.h>
+#import "KernelDriver.h" // Importa o seu motor de exploit
+#import <WebKit/WebKit.h>
+
+@interface ViewController ()
+@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) KernelBridge *bridge; // Instância do driver
+@end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // 1. Forçar carregamento da dylib (GitHub/Feather)
-    void *handle = dlopen("@executable_path/KernelBridge.dylib", RTLD_NOW);
-    if (!handle) handle = dlopen("KernelBridge.dylib", RTLD_NOW);
-
-    // 2. CONFIGURAÇÃO CRÍTICA: Registrar o handler ANTES da WebView
-    WKUserContentController *userContent = [[WKUserContentController alloc] init];
     
-    // Tenta achar a classe KernelBridge que compilamos no GitHub
-    Class bridgeClass = NSClassFromString(@"KernelBridge");
-    if (bridgeClass) {
-        id bridgeInstance = [[bridgeClass alloc] init];
-        
-        // IMPORTANTE: O nome aqui DEVE ser "kexec" para bater com o seu JS
-        [userContent addScriptMessageHandler:bridgeInstance name:@"kexec"];
-        
-        // Conecta a webview à bridge para o C poder mandar logs de volta
-        // Usamos delay porque a webview ainda não foi criada
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [bridgeInstance setValue:self.webView forKey:@"webView"];
-        });
-    }
-
+    // 1. Inicializa o motor do Kernel
+    self.bridge = [[KernelBridge alloc] init];
+    
+    // 2. Configura a WebView e a ponte de comunicação
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-    config.userContentController = userContent;
-
+    // Registra o bridge para responder às mensagens do JS chamadas "kernel"
+    [config.userContentController addScriptMessageHandlerWithReply:self.bridge 
+                                                      contentWorld:WKContentWorld.pageWorld 
+                                                              name:@"kernel"];
+    
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
+    self.webView.backgroundColor = [UIColor blackColor];
     [self.view addSubview:self.webView];
-
-    // 3. Carregar o HTML
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
-    if (path) {
-        NSURL *url = [NSURL fileURLWithPath:path];
-        [self.webView loadFileURL:url allowingReadAccessToURL:[url URLByDeletingLastPathComponent]];
+    
+    // 3. Carrega o HTML (index.html) que criamos
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
+    if (url) {
+        [self.webView loadFileURL:url allowingReadAccessToURL:url];
     }
 }
+
+// Garante que a WebView ocupe a tela toda no iPhone 11
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.webView.frame = self.view.bounds;
+}
+
 @end
