1205c794e1183f66e7171f6b675c92ba03e29e94
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 19:53:01 -0300

    Update KernelDriver.h

--- KernelDriver.h
+++ KernelDriver.h
@@ -1,22 +1,10 @@
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface KernelDriver : NSObject <WKScriptMessageHandlerWithReply>
 
-// Primitivas de Exploração
-- (uint64_t)kread64:(uint64_t)addr;
-- (uint64_t)getKernelSlide;
-- (uint64_t)get_pte_for_address:(uint64_t)vaddr;
-- (uint64_t)get_my_ucred_ptr;
-- (void)ppl_write_race:(uint64_t)vaddr value:(uint64_t)val;
-
-// Handler obrigatório para habilitar postMessageWithReply no JS
 - (void)userContentController:(WKUserContentController *)userContentController 
       didReceiveScriptMessage:(WKScriptMessage *)message 
-                 replyHandler:(void (^)(id _Nullable reply, NSString * _Nullable errorMessage))replyHandler;
+                 replyHandler:(void (^)(id reply, NSString * errorMessage))replyHandler;
 
 @end
-
-NS_ASSUME_NONNULL_END
