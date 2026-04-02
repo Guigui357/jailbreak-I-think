82753ee4c1f19cb4682b59cea6b2a86610d4dc4a
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 19:24:57 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -2,55 +2,29 @@
 #import "KernelDriver.h"
 
 @interface ViewController ()
-
-@property (strong, nonatomic) KernelDriver *driver; // Retenção FORTE é obrigatória
+@property (strong, nonatomic) KernelDriver *driver;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // 1. Instanciar o Driver (O objeto que processa o exploit)
     _driver = [[KernelDriver alloc] init];
     
-    // 2. Configurar o UserContentController
     WKUserContentController *userContentController = [[WKUserContentController alloc] init];
     
-    /** 
-     * CRITICAL: 
-     * O método 'addScriptMessageHandlerWithReply' é o que injeta 
-     * a função 'postMessageWithReply' no JavaScript. 
-     * O uso do 'pageWorld' garante que o script do HTML tenha acesso.
-     */
+    // O SEGREDO: addScriptMessageHandlerWithReply + pageWorld
     [userContentController addScriptMessageHandlerWithReply:self.driver 
                                                contentWorld:[WKContentWorld pageWorld] 
                                                        name:@"kernel"];
     
-    // 3. Configurar a WebView
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     config.userContentController = userContentController;
     
-    // Permitir leitura de arquivos do Bundle (importante para o sshd_static)
-    [config.preferences setValue:@YES forKey:@"allowFileAccessFromFileURLs"];
-    
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
-    self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    
     [self.view addSubview:self.webView];
     
-    // 4. Carregar o Console de Lab (index.html)
     NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
-    if (url) {
-        [self.webView loadFileURL:url allowingReadAccessToURL:url.URLByDeletingLastPathComponent];
-    } else {
-        NSLog(@"[!] ERRO: index.html não encontrado no Bundle.");
-    }
+    [self.webView loadFileURL:url allowingReadAccessToURL:url.URLByDeletingLastPathComponent];
 }
-
-// Ocultar a barra de status para o console ocupar a tela toda
-- (BOOL)prefersStatusBarHidden {
-    return YES;
-}
-
 @end
