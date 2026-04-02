99f8762ea0eeb9e440803678ea7cd63ad7b53437
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 20:16:00 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -13,23 +13,23 @@
     
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     
-    // Injeção manual para garantir que o objeto não seja undefined
-    NSString *jsInject = @"window.webkit.messageHandlers.kernel = window.webkit.messageHandlers.kernel || {};";
-    WKUserScript *script = [[WKUserScript alloc] initWithSource:jsInject 
-                                                  injectionTime:WKUserScriptInjectionTimeAtDocumentStart 
-                                               forMainFrameOnly:YES];
-    [config.userContentController addUserScript:script];
+    // Injeção EXTREMA: Força o objeto existir no JS antes de qualquer coisa
+    WKUserScript *s = [[WKUserScript alloc] initWithSource:@"window.kernel = window.webkit.messageHandlers.kernel;" 
+                                              injectionTime:WKUserScriptInjectionTimeAtDocumentStart 
+                                           forMainFrameOnly:YES];
+    [config.userContentController addUserScript:s];
 
-    // Injeção da Ponte Real
     [config.userContentController addScriptMessageHandlerWithReply:self.driver 
                                                       contentWorld:[WKContentWorld pageWorld] 
                                                               name:@"kernel"];
-    
+
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
     [self.view addSubview:self.webView];
     
-    NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
-    [self.webView loadFileURL:url allowingReadAccessToURL:url.URLByDeletingLastPathComponent];
+    // Carrega o HTML do Bundle como string para evitar restrições de 'file://'
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+    NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+    [self.webView loadHTMLString:html baseURL:[[NSBundle mainBundle] bundleURL]];
 }
 
 @end
