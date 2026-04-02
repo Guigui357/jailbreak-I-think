1eb2df1d30548fbd23a06d1f8779e54f7d91cb5b
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 17:24:42 -0300

    Update KernelDriver.h

--- KernelDriver.h
+++ KernelDriver.h
@@ -1,17 +1,14 @@
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
-#import <mach/mach.h>
 
-#define PAGE_SIZE_A13 0x4000
-#define KERNEL_BASE_STATIC 0xFFFFFFF007004000
+#define ARM_TTE_PTE_MASK            0x0000FFFFFFFFF000ULL
+#define ARM_PTE_AP_RO               (1ULL << 6)   // Bit de Read-Only
+#define ARM_PTE_NX                  (1ULL << 54)  // Execute Never
+#define ARM_PTE_PNX                 (1ULL << 53)  // Privileged Execute Never
 
 @interface KernelBridge : NSObject <WKScriptMessageHandlerWithReply>
-@property (nonatomic, assign) mach_port_t g_host_priv;
-@property (nonatomic, weak) WKWebView *webView;
-
 - (uint64_t)kread64:(uint64_t)addr;
 - (void)kwrite64:(uint64_t)addr value:(uint64_t)val;
-- (uint64_t)getKernelSlide;
-- (uint64_t)findProcByName:(NSString *)name;
-- (void)escalatePrivileges;
+- (uint64_t)get_pte_for_address:(uint64_t)vaddr;
+- (void)patch_pte_make_writable:(uint64_t)vaddr;
 @end
