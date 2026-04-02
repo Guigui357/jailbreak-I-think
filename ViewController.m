67abb9711a528027d5dae075fb277ca1fd69cfff
Guigui357 <zebuchinho77@gmail.com>
2026-04-02 17:49:11 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -5,29 +5,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // 1. Instância do Driver
+    
+    // 1. Inicializa o motor (KernelBridge)
+    // Certifique-se de que a variável 'kernelBridge' está declarada no ViewController.h
     self.kernelBridge = [[KernelBridge alloc] init];
-
-    // 2. Configuração da WebView
+    
+    // 2. Configura a WebView com o Handler "kernel"
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     
-    // IMPORTANTE: O nome aqui DEVE ser "kernel" (minúsculo) para bater com o JS
+    // O SEGREDO: Registrar ANTES de criar a WebView
+    // O nome "kernel" deve ser exatamente igual ao usado no JS
     [config.userContentController addScriptMessageHandlerWithReply:self.kernelBridge 
                                                       contentWorld:WKContentWorld.pageWorld 
                                                               name:@"kernel"];
-
+    
+    // 3. Cria a WebView ocupando a tela toda
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
-    
-    // 3. Permite acesso a arquivos locais (index.html)
-    [self.webView.configuration.preferences setValue:@YES forKey:@"allowFileAccessFromFileURLs"];
-    
+    self.webView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
     [self.view addSubview:self.webView];
-
-    // 4. Carrega o HTML
+    
+    // 4. Carrega o HTML do Bundle
     NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
     if (url) {
+        // 'allowingReadAccessToURL' permite que o HTML leia arquivos na mesma pasta
         [self.webView loadFileURL:url allowingReadAccessToURL:url.URLByDeletingLastPathComponent];
+        NSLog(@"[+] Bridge 'kernel' injetada e index.html carregado.");
+    } else {
+        NSLog(@"[!] ERRO: index.html não encontrado no projeto Xcode.");
     }
 }
+
 @end
