b5ca478ffde424b9018c334e8d731b886ceb7234
Guigui357 <zebuchinho77@gmail.com>
2026-04-03 09:34:30 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -6,30 +6,27 @@
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.driver = [[KernelDriver alloc] init];
     
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     
-    // Injeção EXTREMA: Força o objeto existir no JS antes de qualquer coisa
+    // Injeção manual para garantir visibilidade do objeto no JS
     WKUserScript *s = [[WKUserScript alloc] initWithSource:@"window.kernel = window.webkit.messageHandlers.kernel;" 
-                                              injectionTime:WKUserScriptInjectionTimeAtDocumentStart 
-                                           forMainFrameOnly:YES];
+                                              injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES];
     [config.userContentController addUserScript:s];
 
+    // USAR defaultWorld para arquivos locais/strings no iOS 26.4
     [config.userContentController addScriptMessageHandlerWithReply:self.driver 
-                                                      contentWorld:[WKContentWorld pageWorld] 
-                                                              name:@"kernel"];
+                                                      contentWorld:[WKContentWorld defaultWorld] name:@"kernel"];
 
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
     [self.view addSubview:self.webView];
     
-    // Carrega o HTML do Bundle como string para evitar restrições de 'file://'
+    // CARREGAR COMO STRING: Burlar restrições de 'file://' do A13
     NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
     NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
     [self.webView loadHTMLString:html baseURL:[[NSBundle mainBundle] bundleURL]];
 }
-
 @end
