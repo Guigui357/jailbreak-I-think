784e1570798ee1609faa8c95f6ad30f1dd2e6a35
Guigui357 <zebuchinho77@gmail.com>
2026-04-03 14:39:39 -0300

    Update ViewController.h

--- ViewController.h
+++ ViewController.h
@@ -1,6 +1,19 @@
+//
+//  ViewController.h
+//  JailbreakApp
+//
+
 #import <UIKit/UIKit.h>
 #import <WebKit/WebKit.h>
 
 @interface ViewController : UIViewController
+
 @property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UITextView *consoleView;
+@property (nonatomic, strong) UITextField *commandField;
+
+- (void)log:(NSString *)message;
+- (void)executeCommand;
+- (void)runExploit;
+
 @end
