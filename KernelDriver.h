ddb8139fa949028cd33de73cce51cd9da69d75e2
Guigui357 <zebuchinho77@gmail.com>
2026-04-03 11:01:33 -0300

    Update KernelDriver.h

--- KernelDriver.h
+++ KernelDriver.h
@@ -1,13 +1,9 @@
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
 
-@interface KernelDriver : NSObject <WKScriptMessageHandler>
+// Adicione exatamente esta linha:
+@interface KernelDriver : NSObject <WKScriptMessageHandlerWithReply>
 
-// Referência à WebView para enviar a resposta de volta
 @property (nonatomic, weak) WKWebView *webView;
-
-- (uint64_t)kread64:(uint64_t)addr;
-- (uint64_t)getKernelSlide;
-- (void)ppl_write_race:(uint64_t)vaddr value:(uint64_t)val;
 
 @end
