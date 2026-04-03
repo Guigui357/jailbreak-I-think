9486d2f3265fef8c2f80e2aee0a0be8dc9153349
Guigui357 <zebuchinho77@gmail.com>
2026-04-03 09:41:56 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -1,27 +1,45 @@
+#import "ViewController.h"
+#import "KernelDriver.h"
+
+@interface ViewController ()
+// Retenção FORTE para a ponte não cair
+@property (strong, nonatomic) KernelDriver *driver; 
+@end
+
+@implementation ViewController
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // 1. Manter referência forte
+    // 1. Instância do Driver
     self.driver = [[KernelDriver alloc] init];
     
+    // 2. Configuração da WebView
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     
-    // 2. INJEÇÃO: No iOS 26.4, se omitirmos o 'contentWorld', o WebKit 
-    // usa o contexto compatível com o JavaScript da página por padrão.
+    // Injeção manual via UserScript para garantir visibilidade no JS
+    WKUserScript *s = [[WKUserScript alloc] initWithSource:@"window.kernel = window.webkit.messageHandlers.kernel;" 
+                                              injectionTime:WKUserScriptInjectionTimeAtDocumentStart 
+                                           forMainFrameOnly:YES];
+    [config.userContentController addUserScript:s];
+
+    // No iOS 26.4, omitir o contentWorld no addScript ajuda na compatibilidade do A13
     [config.userContentController addScriptMessageHandlerWithReply:self.driver 
                                                               name:@"kernel"];
-    
-    // 3. Habilitar TUDO para o exploit
+
+    // 3. Permissões de Sandbox
     [config.preferences setValue:@YES forKey:@"allowFileAccessFromFileURLs"];
     [config setValue:@YES forKey:@"allowUniversalAccessFromFileURLs"];
-    
-    // 4. Inicializar WebView
+
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
     [self.view addSubview:self.webView];
     
-    // 5. CARREGAMENTO: O segredo para o erro 'is not a function' sumir 
-    // é carregar via baseURL do Bundle, mas como String.
+    // 4. Carregar o HTML como String para evitar restrições de 'file://'
     NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
-    NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-    [self.webView loadHTMLString:html baseURL:[[NSBundle mainBundle] bundleURL]];
+    if (path) {
+        NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+        [self.webView loadHTMLString:html baseURL:[[NSBundle mainBundle] bundleURL]];
+    }
 }
+
+@end
