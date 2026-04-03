e2cd5f0365261648977d87fe1977656371979706
Guigui357 <zebuchinho77@gmail.com>
2026-04-03 10:47:42 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -1,37 +1,47 @@
-#import "ViewController.h"
+##import "ViewController.h"
 #import "KernelDriver.h"
 
 @interface ViewController ()
-@property (strong, nonatomic) KernelDriver *driver; 
+@property (strong, nonatomic) KernelDriver *driver;
+@property (strong, nonatomic) WKWebView *webView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // 1. Inicializa o Driver
     self.driver = [[KernelDriver alloc] init];
     
+    // 2. Configura a comunicação WebKit
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     
-    // 1. INJEÇÃO MANUAL: Força o objeto existir no JS antes do HTML carregar
+    // Injeção de UserScript para facilitar o acesso no JS
     NSString *js = @"window.A13_LAB = window.webkit.messageHandlers.A13_LAB;";
-    WKUserScript *s = [[WKUserScript alloc] initWithSource:js 
-                                              injectionTime:WKUserScriptInjectionTimeAtDocumentStart 
-                                           forMainFrameOnly:YES];
-    [config.userContentController addUserScript:s];
+    WKUserScript *script = [[WKUserScript alloc] initWithSource:js 
+                                                injectionTime:WKUserScriptInjectionTimeAtDocumentStart 
+                                             forMainFrameOnly:YES];
+    [config.userContentController addUserScript:script];
 
-    // 2. REGISTRO: Nome único para evitar filtros do sistema
-    [config.userContentController addScriptMessageHandler:self.driver name:@"A13_LAB"];
+    // REGISTRO CRÍTICO: Usa 'addScriptMessageHandlerWithReply' para iOS 14+
+    if (@available(iOS 14.0, *)) {
+        [config.userContentController addScriptMessageHandlerWithReply:self.driver 
+                                                        contentWorld:[WKContentWorld pageWorld] 
+                                                                name:@"A13_LAB"];
+    }
 
+    // 3. Inicializa a WebView
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
-    self.driver.webView = self.webView; // Importante para o Callback
+    self.driver.webView = self.webView; // Referência para callbacks se necessário
     [self.view addSubview:self.webView];
     
-    // 3. CARREGAMENTO: O iOS 26.4 bloqueia handlers em file:// 
-    // Carregar como String remove a restrição de 'Origin' do WebKit
+    // 4. Carrega o HTML (index.html deve estar no Bundle)
     NSString *path = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
-    NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-    [self.webView loadHTMLString:html baseURL:[[NSBundle mainBundle] bundleURL]];
+    if (path) {
+        NSString *html = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+        [self.webView loadHTMLString:html baseURL:[[NSBundle mainBundle] bundleURL]];
+    }
 }
 
 @end
