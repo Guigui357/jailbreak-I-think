19d65988fdf66c6f171132b6e80d52cac6a9242d
Guigui357 <zebuchinho77@gmail.com>
2026-04-03 09:32:06 -0300

    Update KernelDriver.h

--- KernelDriver.h
+++ KernelDriver.h
@@ -3,8 +3,14 @@
 
 @interface KernelDriver : NSObject <WKScriptMessageHandlerWithReply>
 
+- (uint64_t)kread64:(uint64_t)addr;
+- (uint64_t)getKernelSlide;
+- (uint64_t)get_pte_for_address:(uint64_t)vaddr;
+- (uint64_t)get_my_ucred_ptr;
+- (void)ppl_write_race:(uint64_t)vaddr value:(uint64_t)val;
+
 - (void)userContentController:(WKUserContentController *)userContentController 
       didReceiveScriptMessage:(WKScriptMessage *)message 
-                 replyHandler:(void (^)(id reply, NSString * errorMessage))replyHandler;
+                 replyHandler:(void (^)(id reply, NSString *errorMessage))replyHandler;
 
 @end
