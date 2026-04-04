a2d5b7bd2d2f3a89772b20461d410f2753f82572
Guigui357 <zebuchinho77@gmail.com>
2026-04-04 18:23:07 -0300

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -2,7 +2,6 @@
 #import <WebKit/WebKit.h>
 #import "KernelDriver.h"
 
-// Adicionando conformidade ao protocolo para aceitar o 'self' como handler
 @interface ViewController () <WKScriptMessageHandler>
 @end
 
@@ -13,8 +12,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Inicializa o motor de kernel (A13/TFP0)
     _driver = [[KernelDriver alloc] init];
 
+    // Configura a ponte para o JavaScript
     WKUserContentController *ucc = [[WKUserContentController alloc] init];
     [ucc addScriptMessageHandler:self name:@"A13_LAB"];
 
@@ -24,26 +26,52 @@
     _webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:cfg];
     [self.view addSubview:_webView];
 
+    // Carrega o index.html do Bundle
     NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
     if (url) [_webView loadFileURL:url allowingReadAccessToURL:url];
 }
 
-- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
+// --- BRIDGE: RECEBENDO MENSAGENS DO INDEX.HTML ---
+- (void)userContentController:(WKUserContentController *)ucc didReceiveScriptMessage:(WKScriptMessage *)message {
     NSDictionary *data = message.body;
     NSString *action = data[@"action"];
-
+    
+    // 1. AÇÃO DE ESCRITA NO KERNEL (PATCH AMFI/PAC)
     if ([action isEqualToString:@"kwrite"]) {
         uint64_t addr = (uint64_t)[data[@"addr"] longLongValue];
         uint64_t val = (uint64_t)[data[@"val"] longLongValue];
         [_driver kwrite64:addr value:val];
-    } else if ([action isEqualToString:@"shell"]) {
-        [_driver executeShell:data[@"payload"]];
+    }
+    
+    // 2. AÇÃO DE SHELL (COMANDOS ROOT)
+    else if ([action isEqualToString:@"shell"]) {
+        NSString *payload = data[@"payload"];
+        NSString *output = @"";
+
+        // SE O COMANDO FOR 'sshd', BUSCAMOS O CAMINHO NO BUNDLE
+        if ([payload isEqualToString:@"sshd"]) {
+            NSString *sshPath = [[NSBundle mainBundle] pathForResource:@"sshd" ofType:nil];
+            if (sshPath) {
+                // Comando real: /caminho/do/app/sshd -p 2222 -R (para rodar em background)
+                NSString *fullCmd = [NSString stringWithFormat:@"%@ -p 2222 -R", sshPath];
+                output = [_driver executeShell:fullCmd];
+            } else {
+                output = @"Erro: Binário sshd não encontrado no Bundle.";
+            }
+        } else {
+            // Comando comum (id, ls, mount, etc)
+            output = [_driver executeShell:payload];
+        }
+
+        // RETORNA A SAÍDA PARA O TERMINAL DO HTML
+        NSString *js = [NSString stringWithFormat:@"log('%@', 'success')", [output stringByReplacingOccurrencesOfString:@"\n" withString:@"<br>"]];
+        [_webView evaluateJavaScript:js completionHandler:nil];
     }
 }
 
-// Implementando os métodos que o seu Header (.h) exigia para não dar warning
+// Métodos exigidos pelo Header
 - (void)log:(NSString *)message { NSLog(@"%@", message); }
-- (void)executeCommand { [self log:@"Comando executado"]; }
-- (void)runExploit { [self log:@"Exploit iniciado"]; }
+- (void)executeCommand { }
+- (void)runExploit { }
 
 @end
