302bcf4600a4a83f335982393038805d8737b3cd
Guigui357 <zebuchinho77@gmail.com>
2026-04-04 18:24:09 -0300

    Update KernelDriver.h

--- KernelDriver.h
+++ KernelDriver.h
@@ -2,5 +2,5 @@
 
 @interface KernelDriver : NSObject
 - (void)kwrite64:(uint64_t)addr value:(uint64_t)val;
-- (void)executeShell:(NSString *)cmd;
+- (NSString *)executeShell:(NSString *)cmd; // Alterado para NSString
 @end
