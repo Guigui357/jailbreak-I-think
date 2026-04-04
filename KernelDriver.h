3cc6c65c740513c90b69cb8e93a49adfe75d53f6
Guigui357 <zebuchinho77@gmail.com>
2026-04-04 18:33:40 -0300

    Update KernelDriver.h

--- KernelDriver.h
+++ KernelDriver.h
@@ -1,6 +1,17 @@
 #import <Foundation/Foundation.h>
 
 @interface KernelDriver : NSObject
+
+// Primitiva de escrita física (Bypass PPL/AMCC)
 - (void)kwrite64:(uint64_t)addr value:(uint64_t)val;
-- (NSString *)executeShell:(NSString *)cmd; // Alterado para NSString
+
+// Executa comandos genéricos do sistema via posix_spawn
+- (NSString *)executeShell:(NSString *)cmd;
+
+// Geração de chaves de Host para o Dropbear (RSA/ECDSA)
+- (NSString *)generateSSHKeys;
+
+// Inicializa o daemon SSHD (Dropbear) na porta 2222
+- (NSString *)executeSSHD;
+
 @end
